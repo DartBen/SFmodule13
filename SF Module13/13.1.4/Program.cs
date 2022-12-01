cb41dcb8ccb15a8f81411202739bd06e5780629e
Dmitry <ryabchikk@rambler.ru>
2022-12-01 23:28:02 +0300

    13.1.6 init

--- SF Module13/13.1.4/Program.cs
+++ SF Module13/13.1.4/Program.cs
@@ -4,4 +4,16 @@
     {
         Console.WriteLine("Hello, World!");
     }
+
+    public bool IsSorted(int[] ints)
+    {
+        for (int a = 0; a < ints.Length - 1; a++)
+        {
+            if (ints[a] > ints[a + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
