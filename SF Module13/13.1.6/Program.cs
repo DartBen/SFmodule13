b19918975a837d8b5de95d904afe3968906c6f1f
Dmitry <ryabchikk@rambler.ru>
2022-12-03 21:36:16 +0300

    13.1.6

--- SF Module13/13.1.6/Program.cs
+++ SF Module13/13.1.6/Program.cs
@@ -1,7 +1,50 @@
-﻿internal class Program
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using static System.Console;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
 {
     private static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello, World!");
+        string path = "C:\\Users\\Dima\\Google Диск\\Repos\\SF Module 13\\cdev_Text.txt";
+        string[] spliters = { " ", "\r", "\n" };
+
+        try
+        {
+            //using (FileStream fsSource = new FileStream(path, FileMode.Open, FileAccess.Read))
+            //{
+            //    string[] readText = File.ReadAllLines(path);
+            //    StringBuilder strBuilder= new StringBuilder();
+
+            //    foreach (string str in readText)
+            //    {
+            //        strBuilder.Append(str);
+            //    }
+
+            //    string text = strBuilder.ToString();
+            //    string[] Test = text.Split(spliters, StringSplitOptions.RemoveEmptyEntries);
+
+            //    WriteLine ($"Длина массива: {Test.Length}"); 
+            //    ReadKey();
+            //}
+
+
+
+            string text = File.ReadAllText(path);
+            var Test = text.Split(spliters, StringSplitOptions.RemoveEmptyEntries);
+            WriteLine($"Длина массива: {Test.Length}");
+
+        }
+        catch (Exception exception)
+        {
+            WriteLine(exception.Message);
+        }
+
+
+
+
     }
 }
