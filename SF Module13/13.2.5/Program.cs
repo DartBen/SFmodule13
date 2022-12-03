6c844bddfc06ef8994ac2c8a0d11417cb07109d6
Dmitry <ryabchikk@rambler.ru>
2022-12-03 23:13:43 +0300

    13.2.6

---
+++ SF Module13/13.2.5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var months = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+        var numbers = new[] { 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+
+        //var arrList = new ArrayList();
+
+        //Console.WriteLine(months.Length);
+        //Console.WriteLine(numbers.Length);
+
+        //if (months.Length == numbers.Length)
+        //{
+        //    for (int i = 0; i < months.Length; i++)
+        //    {
+        //        arrList.Add(months[i]);
+        //        arrList.Add(numbers[i]);
+        //    }
+        //}
+
+        //foreach(var a in arrList)
+        //{
+        //    Console.WriteLine(a);
+        //}
+
+
+        /* Не согласен с заданием- если нужно чтобы за каждым месяцем шло его числовое значение то апрель нельзя пропускать*/
+        var combinedList = new ArrayList();
+
+        //  пробегаемся по массиву чисел
+        foreach (var number in numbers)
+        {
+            // добавляем в ArrayList строку месяца (начинаем с нулевого по индексу)
+            combinedList.Add(months[number - 1]);
+
+            // добавляем его порядковый номер
+            combinedList.Add(number);
+        }
+
+        foreach (var value in combinedList)
+            Console.WriteLine(value);
+    }
+}
---
+++ SF Module13/13.2.6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    public ArrayList ArrListCovert(ArrayList input)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        int resultCount = 0;
+
+        foreach (var item in input)
+        {
+            if (item is string)
+            {
+                stringBuilder.Append(item);
+            }
+            else if (item is int)
+            {
+                resultCount += (int)item;
+            }
+
+        }
+
+        var str = stringBuilder.ToString();
+        ArrayList result = new ArrayList() { resultCount, str };
+
+
+        return result;
+
+    }
+
+}
